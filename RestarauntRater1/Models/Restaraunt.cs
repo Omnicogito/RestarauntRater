db1ec34897d4a750077bd9a85f0c4f7fe6816c53
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-03-25 11:52:00 -0400

    Added Restaraunt and RestarauntDBContext

--- RestarauntRater1/Models/Restaraunt.cs
+++ RestarauntRater1/Models/Restaraunt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,14 @@ namespace RestarauntRater1.Models
 {
     public class Restaraunt
     {
+        public int RestarauntID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public double Rating { get; set; }
+    }
 
+    public class RestarauntDBContext : DbContext
+    {
+        public DbSet<Restaraunt> Restaraunts { get; set; }
     }
 }
