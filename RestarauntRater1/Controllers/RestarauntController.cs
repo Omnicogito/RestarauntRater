59d3e602c8612dffc30634728fbaf05bf2763a93
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-03-25 12:07:28 -0400

    Added Restaraunt Index

---
+++ RestarauntRater1/Controllers/RestarauntController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestarauntRater1.Controllers
+{
+    public class RestarauntController : Controller
+    {
+        // GET: Restaraunt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
