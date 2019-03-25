3ae50ee34a0586406060e63ef2550da56326dc14
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-03-25 11:37:46 -0400

    Initial Commit

---
+++ RestarauntRater1/Models/Restaraunt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestarauntRater1.Models
+{
+    public class Restaraunt
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+
+    }
+}
