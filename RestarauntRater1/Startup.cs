b3cc1fd8362597f8e304f46005b4c3f0af8ff9ba
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-03-25 11:36:12 -0400

    Add project files.

---
+++ RestarauntRater1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestarauntRater1.Startup))]
+namespace RestarauntRater1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
