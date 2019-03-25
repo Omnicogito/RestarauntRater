4b149f6322c4c5a5b340e10ca048a64d14e23c18
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-03-25 12:41:49 -0400

    Added table to Restaraunt page

--- RestarauntRater1/Controllers/RestarauntController.cs
+++ RestarauntRater1/Controllers/RestarauntController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestarauntRater1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace RestarauntRater1.Controllers
 {
     public class RestarauntController : Controller
     {
+        private RestarauntDBContext db = new RestarauntDBContext();
         // GET: Restaraunt
         public ActionResult Index()
         {
-            return View();
+            return View(db.Restaraunts.ToList());
         }
     }
 }
